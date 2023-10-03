6d5486d7da95d33dc66b4a7041051c044b9c656f
Rajo Gajic <80322468+rgajic@users.noreply.github.com>
2023-10-03 16:39:30 +0200

    Update main.tf

--- modules/http_server/main.tf
+++ modules/http_server/main.tf
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
+e
 locals {
   network = "${element(split("-", var.subnet), 0)}"
 }
@@ -21,7 +21,7 @@ resource "google_compute_instance" "http_server" {
   project      = "${var.project}"
   zone         = "us-west1-b"
   name         = "${local.network}-apache2-instance"
-  machine_type = "f1-micro"
+  machine_type = "e2-micro"
 
   metadata_startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && echo '<html><body><h1>Environment: ${local.network}</h1></body></html>' | sudo tee /var/www/html/index.html"
 
